5d87c567603356852492555838d2e56ffe9e7c2e
AroniasPrenovost <Aroniascoder@gmail.com>
2017-01-07 16:38:09 -0800

    serve stylesheets statically with nginx.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// confirms static mappings are configured correctly 
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
